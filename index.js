e66ab13e65f787abfc9b3f85995035a8662987fd
goodwyyn <godwin.mtuma@student.moringaschool.com>
2022-06-16 10:41:40 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const varCheck = "hi"
+
+document.querySelector("main#main").remove();
+
+const newHeader = document.createElement("hi");
+
+newHeader.id="victory"
+newHeader.innerHTML="Godwin is the champion!"
+newHeader.className="victory";
